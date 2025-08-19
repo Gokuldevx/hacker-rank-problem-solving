680edca09f3ba699be9af55e6b038f074702e2d1
Gokuldevx <gokuldevse2001@gmail.com>
2025-08-19 11:03:50 +0530

    initial commit

---
+++ aVeryBigSum.py
@@ -0,0 +1,11 @@
+def aVeryBigSum(ar):
+    # Write your code here
+    return sum(ar)
+
+if __name__ == '__main__':
+   
+    ar_count = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+    print(result)
